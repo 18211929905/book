8b9ecb14819d154984794c297b8e0f3cbef9da03
unknown <820932808@qq.com>
2018-11-03 00:12:36 +0800

    修改提交

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <span class="icon-bookmark"></span>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  // name: 'home',
+  //   mounted () {
+  //     this.book = '1'
+  //       console.log(this.book)
+  //   }
+}
+</script>
