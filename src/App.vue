6b80585ddccd0eecb1f73f7ba1b5a8dc279309f6
unknown <820932808@qq.com>
2018-11-07 17:50:45 +0800

    11.7

--- src/App.vue
+++ src/App.vue
@@ -8,37 +8,14 @@
 
 <script>
 import { mapGetters } from "vuex";
-const getters = {
-  a:function(){
-    return 1
-  },
-  b:function(){
-    return 2
-  }
-};
-function fn(keys) {
-  console.log(getters)
-  const data = {}
-  keys.forEach(key => {
-    if(getters.hasOwnProperty(key)){
-      data[key] = getters[key]
-    }
-  });
-  return data
-}
 export default {
-  computed: {
-    ...fn(["a", "b"]),
-    ...mapGetters(["test"])
-  },
-  mounted() {
-    // this.$store.dispatch("setTest", 10).then(() => {
-    //   console.log(this.test);
-    // });
-    console.log(this.a,this.b);
-  }
 };
 </script>
 
-<style>
+<style scoped>
+  #app{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
 </style>
---
+++ src/views/ebook/index.vue
@@ -0,0 +1,16 @@
+<template>
+        <ebook-reader></ebook-reader>
+</template>
+
+<script>
+import EbookReader from '../../components/ebook/ebook-reader.vue'
+export default {
+    components: {
+        EbookReader
+    }
+}
+</script>
+
+<style>
+
+</style>
